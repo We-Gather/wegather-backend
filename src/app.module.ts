babb751fbdfadee02318c5eaae09920edc9c2d10
0816i <0816i@naver.com>
2023-08-12 14:16:55 +0900

    fix: CoreModule 로 lifecycle 통제 하도록 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { CoreModule } from '@app/core/CoreModule';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+  imports: [PrismaModule, AuthModule, UsersModule, CoreModule],
   controllers: [],
   providers: [],
 })
