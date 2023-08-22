c62d31894d84f7dbd309c009ca8f7d5ea13a305e
mathpaul3 <mathpaul3@gmail.com>
2023-08-22 23:06:41 +0900

    Test 작성

--- src/club/club.service.spec.ts
+++ src/club/club.service.spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClubService } from './club.service';
+import { DatabaseService } from '@app/core/database/DatabaseService';
 
 describe('ClubService', () => {
   let service: ClubService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClubService],
+      providers: [ClubService, DatabaseService],
     }).compile();
     service = module.get<ClubService>(ClubService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('create Club', async () => {
+    const result: number = await service.create({
+      name: '동아리1',
+      type: '일반동아리',
+      school_id: 1,
+      introduction: '',
+    });
+    expect(typeof result).toEqual('number');
   });
 });
